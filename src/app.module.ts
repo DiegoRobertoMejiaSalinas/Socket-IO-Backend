a8bfe880f369cf148525bf25c59cd5a5166dfeea
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-02-26 20:40:58 -0500

    chore: Imported Database Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './core/database/database.module';
+import { databaseConfig as postgresConfig } from './core/database/infrastructure/database.postgres';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    DatabaseModule.forRoot(postgresConfig),
     ConfigModule.forRoot({
       envFilePath: ['.env'],
     }),
