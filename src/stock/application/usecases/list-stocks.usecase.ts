31a9f002ab73ffab69110e7002b1af983143af9b
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-03-01 01:56:31 -0500

    chore: List Stocks Usecase created

---
+++ src/stock/application/usecases/list-stocks.usecase.ts
@@ -0,0 +1,36 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StockEntity } from 'src/stock/domain/entities/stock.entity';
+import { StockRepository } from 'src/stock/infrastructure/repositories/stock.repository';
+
+@Injectable()
+export class ListStocksUsecase {
+  constructor(
+    @InjectRepository(StockEntity)
+    private readonly _stockRepository: StockRepository,
+  ) {}
+
+  async handle() {
+    try {
+      const foundStocks = await this._stockRepository.find({
+        relations: {
+          purchases: true,
+        },
+      });
+
+      return {
+        success: true,
+        data: foundStocks
+      }
+    } catch (e) {
+      if (e?.response?.error?.type) throw e;
+
+      throw new InternalServerErrorException({
+        error: {
+          type: 'error_listing_stocks',
+          message: 'There was an error listing the stocks',
+        },
+      });
+    }
+  }
+}
